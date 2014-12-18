74ec5eaa172e2caf00df3b2ff5017cc41b25fb53
David Johnson <djohnson.m@gmail.com>
2014-12-17 18:04:56 -0600

    added new api examples

--- examples/Person.hs
+++ examples/Person.hs
@@ -1,16 +1,42 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
     
 import           Web.AWS.DynamoDB.Client
 import           Web.AWS.DynamoDB.Types
 import           Web.AWS.DynamoDB.CreateTable
+import           Web.AWS.DynamoDB.UpdateTable
+import           Web.AWS.DynamoDB.DeleteTable
+import           Web.AWS.DynamoDB.ListTables
+import           Web.AWS.DynamoDB.DescribeTable
 
-import qualified Data.Text as T
-import           Data.Text    (Text)
-import           Data.Aeson
-import qualified Data.Text as T
-import           Data.Text    (Text,unpack)
-import           Control.Applicative
+import           Control.Retry  (exponentialBackoff)
+
+import           System.IO.Streams.HTTP
+
+ct :: CreateTable
+ct = CreateTable "foobar" [ Key "id" HASH S ] (Throughput 1 1) [] []
+
+dt :: DescribeTable
+dt = DescribeTable "foobar" 
+
+delt :: DeleteTable
+delt = DeleteTable "foobar" 
+
+ut :: UpdateTable
+ut = UpdateTable "foobar" (Throughput 2 2)
+
+getConfig :: IO DynamoConfig
+getConfig = do
+  let pk = PublicKey "test"
+      sk = SecretKey "test"
+  withManager tlsManagerSettings $ \mgr ->
+     return $ DynamoConfig pk sk mgr (exponentialBackoff 10) UsEast1 True False 
+
+    
+main :: IO ()
+main = do
+  config <- getConfig
+  result <- dynamo config dt
+  print result
 
 
